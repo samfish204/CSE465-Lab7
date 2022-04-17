85d07e58a6add9066dbc61621d207453ba5d3673
fishe108 <fishe108@ceclnx01-19.csi.miamioh.edu>
2022-04-16 22:57:50 -0400

    Submitting

--- lex.cs
+++ lex.cs
@@ -1,6 +1,12 @@
 using System;
 using System.Collections.Generic;
 
+// Edited By: Samuel Fisher
+// CSE 465
+// Dr. Femiani
+// Lab 07
+// Due April 18th
+
 /**
  * This lab is the FIRST part of a parser lab.  This week we focus on lexing. 
  * Eventually, we will build a parser for a simple language that supports assignments
@@ -11,22 +17,24 @@ using System.Collections.Generic;
  *      ID:  Any letter followed be a sequence of letters and numbers
  *      REAL: An optional sign followed by a sequence of digits, optionally with single decimal point. 
  *      WS:  Whitespace (no tokens generated, this is skipped)
- *      LPAREN, RPAREN, EQUALS:  (, ), and = literals
- *      OP_PLUS, OP_MINUS: + and - literals
- *      OP_MULTIPLY, OP_DIVIDE:  * and / literals
- *      OP_EXPONENT: ** literal (x**2 is "x squared"). 
+ *      LIT: (, ), +, -, *, /, =
  *Grammar:
- *      <stmt> ::= <assign> | <expr>
- *      <assign> ::= ID = <expr>
- *      <expr> ::= <term> | <term> + <expr> | <term> - <expr>
- *      <term> ::= <factor> | <factor> * <term> | <factor> / <term>
- *      <factor> ::= <base>**<factor> | <base>
- *      <base> := ID | NUM |  (<expr>)
+ *   <stmt> ::= <assign> | <expr>
+ *   <assign> ::= ID = <expr> 
+ *   <expr> ::= <term> {(+|-) <term>}
+ *   <term> ::= <factor> { (*|/) <factor> }
+ *   <factor> ::= <base>[**<factor>]
+ *   <base> := ID | NUM |  (<expr>)
+
  */
 public class ExpressionParser {
     
-    public enum Symbol
-    {
+    public enum Symbol {
+		  INVALID,
+		  ID,
+		  LITERAL,
+		  NUM,
+		  WS
     }
 
     /**
@@ -38,8 +46,21 @@ public class ExpressionParser {
      * - Tokens are leaf nodes (the array of children should be null)
      * - Needs a constructor with symbol, text, line, and column
      **/
-    public class Node{
-    }
+    public class Node {
+		public string Text;
+		public int Line;
+		public int Column;
+    public Symbol Token;
+		private List<Node> children = new List<Node>();
+		
+		public Node(Symbol token, string text, int line, int column) {
+		  Text = text;
+			Token = token;
+			Line = line;
+			Column = column;
+			children = new List<Node>();
+		}
+  }
 
     /**
      * Generator for tokens. 
@@ -47,18 +68,167 @@ public class ExpressionParser {
      **/
     public static IEnumerable<Node> tokenize(System.IO.StreamReader src) 
     {
-        int line = 1;
-        int column = 1;
-        System.Text.StringBuilder lexeme = new System.Text.StringBuilder();
-
-    
+      int line = 1;
+      int column = 1;
+      System.Text.StringBuilder lexeme = new System.Text.StringBuilder();
+      Symbol token = Symbol.WS;
+		  int state = 0;
+		
+		  while (!src.EndOfStream) {
+        char current = (char)src.Peek();
+			  switch (state) {
+				  case 0:
+					  if (current == '\n') {
+              state = 0;
+							token = Symbol.WS;
+              src.Read();
+							line ++;
+							column = 1;
+            } else if (char.IsWhiteSpace(current)) {
+              token = Symbol.WS;
+              state = 0;
+              src.Read();
+              column++;
+            } else if (current == '+' || current == '-') {
+              token = Symbol.LITERAL;
+              state = 1;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else if (char.IsDigit(current)) {
+              token = Symbol.NUM;
+              state = 2;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else if (current == '*') {
+              token = Symbol.LITERAL;
+              state = 4;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else if (current == '_' || char.IsLetter(current)) {
+              token = Symbol.ID;
+              state = 6;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else if (current == '=' || current == '(' || current == ')') {
+              token = Symbol.LITERAL;
+              state = 5;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else {
+              throw new Exception($"Invalid character '{current}' at line {line} column {column}");
+            }
+					break;
+				  case 1:
+            if (char.IsDigit(current)) {
+              token = Symbol.NUM;
+              state = 2;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else {
+              yield return new Node(token, lexeme.ToString(), line, column);
+              lexeme.Clear();
+              token = Symbol.INVALID;
+              state = 0;
+            }
+					break;
+          case 2: 
+            if (char.IsDigit(current)) {
+              token = Symbol.NUM;
+              state = 2;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else if (current == '.') {
+              token = Symbol.NUM;
+              state = 3;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else {
+              yield return new Node(token, lexeme.ToString(), line, column);
+              lexeme.Clear();
+              token = Symbol.INVALID;
+              state = 0;
+            }
+          break;
+          case 3:
+            if (char.IsDigit(current)) {
+              token = Symbol.NUM;
+              state = 3;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else {
+              yield return new Node(token, lexeme.ToString(), line, column);
+              lexeme.Clear();
+              token = Symbol.INVALID;
+              state = 0;
+            }
+          break;
+          case 4:
+            if (current == '*') {
+              token = Symbol.LITERAL;
+              state = 5;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else {
+              yield return new Node(token, lexeme.ToString(), line, column);
+              lexeme.Clear();
+              token = Symbol.INVALID;
+              state = 0;
+            }
+          break;
+          case 5: 
+            yield return new Node(token, lexeme.ToString(), line, column);
+            lexeme.Clear();
+            token = Symbol.INVALID;
+            state = 0;
+            break;
+          case 6:
+            if (char.IsLetterOrDigit(current) || current == '_') {
+              token = Symbol.ID;
+              state = 6;
+              lexeme.Append(current);
+              src.Read();
+              column++;
+            } else {
+              yield return new Node(token, lexeme.ToString(), line, column);
+              lexeme.Clear();
+              token = Symbol.INVALID;
+              state = 0;
+            }
+          break;
+			  }
+		  }
+		
+      if (token == Symbol.INVALID) {
+        throw new Exception("End of stream before finishing token {token}");
+      } else if (token != Symbol.WS) {
+        yield return new Node(token, lexeme.ToString(), line, column);
+      }
     }
 
-    public static void Main(string[] args){
-        try {
-            foreach (Node n in tokenize(new System.IO.StreamReader(Console.OpenStandardInput()))){
-                Console.WriteLine($"{Enum.GetName(typeof(Symbol), n.symbol),-15}\t{n.text}");
-            }
+    public static void Main(string[] args) {
+      try {
+        System.IO.Stream stream;
+        bool testing = false;
+        if (testing) {
+          string src = "a + b";
+          stream = new System.IO.MemoryStream(System.Text.Encoding.ASCII.GetBytes(src));
+        } else {
+          stream = Console.OpenStandardInput();
+        }
+			// set stream = Console.OpenStandardInput(); when submitting instead of the two lines above ^
+        foreach (Node n in tokenize(new System.IO.StreamReader(stream))){
+          Console.WriteLine($"{n.Token, -15}\t{n.Text}");
+        }
         } catch (Exception e){
             Console.WriteLine(e.Message);
         }
